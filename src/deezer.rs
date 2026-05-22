adf495d6594df9c703bd72ebfcab9153830cccd2
ClementVallois <github.tricky471@passmail.com>
2026-05-22 10:55:41 +0200

    feat: add PlaylistId and PlaylistName newtypes

--- src/deezer.rs
+++ src/deezer.rs
@@ -2,5 +2,24 @@ use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
 pub struct PlaylistDto {
-    pub id: u32,
+    pub id: PlaylistId,
+    pub title: PlaylistName,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize)]
+pub struct PlaylistId(u64);
+
+impl PlaylistId {
+    pub fn value(&self) -> u64 {
+        self.0
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize)]
+pub struct PlaylistName(String);
+
+impl PlaylistName {
+    pub fn value(&self) -> &str {
+        &self.0
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
+mod deezer;
+
+use deezer::PlaylistDto;
 use std::fs;
 
-mod deezer;
-use deezer::PlaylistDto;
-
-const DEEZER_PLAYLIST_FILE_PATH: &str = "./DeezerPlaylist.json";
+const DEEZER_PLAYLIST_FILE_PATH: &str = "DeezerPlaylist.json";
 fn main() -> anyhow::Result<()> {
     let playlist_json = fs::read_to_string(DEEZER_PLAYLIST_FILE_PATH)?;
     let playlist: PlaylistDto = serde_json::from_str(&playlist_json)?;
-    println!("playlist id is: {:#?}", playlist.id);
+    println!("playlist id is: {}", playlist.id.value());
+    println!("playlist title is: {}", playlist.title.value());
     Ok(())
 }
